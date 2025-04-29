2bdf69659a19b346e0160a366d7a361e8f5c3125
Brunoluiz19 <brunolp3@al.insper.edu.br>
2025-04-29 16:24:40 -0300

    ex 3

--- funcoes.py
+++ funcoes.py
@@ -20,3 +20,18 @@ def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     nova_lista_estoque = dados_no_estoque + [novo_dado]
     
     return [nova_lista_rolados, nova_lista_estoque]
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    # Valor do dado que será removido do estoque
+    dado = dados_no_estoque[dado_para_remover]
+    
+    # Cria nova lista de estoque, sem o item no índice dado_para_remover
+    nova_lista_estoque = []
+    for i in range(len(dados_no_estoque)):
+        if i != dado_para_remover:
+            nova_lista_estoque.append(dados_no_estoque[i])
+    
+    # Adiciona o dado de volta aos dados rolados
+    nova_lista_rolados = dados_rolados + [dado]
+    
+    return [nova_lista_rolados, nova_lista_estoque]
