10d9b6de33249fdc00cd0e5a0fbda33b03dfb1c6
erick <erickmoreno150306@gmail.com>
2025-05-02 23:19:21 -0300

    12.1

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,21 @@
 import random
-
+def imprime_cartela(cartela):
+    print("Cartela de Pontos:")
+    print("-"*25)    
+    for i in range(1, 7):
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_simples'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_simples'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    for i in cartela['regra_avancada'].keys():
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_avancada'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_avancada'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    print("-"*25)
+    
 def rolar_dados(qtd_dados):
     resultado = []
     for i in range(qtd_dados):
